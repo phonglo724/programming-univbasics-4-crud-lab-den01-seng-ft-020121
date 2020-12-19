6d6f466356a64abe6d8e9af9392e4eece49ccf25
Phong Lo <phonglo724@gmail.com>
2020-12-19 22:04:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,8 @@ end
 
 def add_element_to_start_of_array(array, element)
   array = ["I", "am", "really", "learning"]
-  array.unshift()
+  array.unshift("wow")
+  p array
 end
 
 def remove_element_from_end_of_array(array)
