a00dea6d27eae0fa8ecb82a8dc9a6b58c9c2d80e
Phong Lo <phonglo724@gmail.com>
2020-12-19 22:12:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  array_remove = remove_element_from_end_of_array.pop
+  array.pop("arrays!")
   p array
 end
 
