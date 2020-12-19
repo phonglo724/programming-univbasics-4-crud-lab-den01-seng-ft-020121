825a519ca658efbb81b74d48bfda6c1f3f69d8a3
Phong Lo <phonglo724@gmail.com>
2020-12-19 21:45:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["#{array}", "#{element}"]
+  ["#{array}"]
   add_element_to_end_of_array << "#{element}"
   p add_element_to_end_of_array
 end
